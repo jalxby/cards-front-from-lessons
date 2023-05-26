47be10e44525a3d326ed3a2aad8fd7736b6c4bed
andres <andres@doersdf.com>
2023-05-26 22:06:57 +0200

    part 4

--- src/features/auth/auth.slice.ts
+++ src/features/auth/auth.slice.ts
@@ -1,43 +1,57 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import {
   AuthApi,
   LoginArgs,
   RegisterArgs,
   User,
 } from "@/features/auth/auth.api"
+import { createAppAsyncThunk } from "@/common"
 
-const register = createAsyncThunk("auth/register", (arg: RegisterArgs) => {
-  AuthApi.register(arg)
-    .then((res) => {
-      console.log(res)
-    })
-    .catch((res) => {
-      console.error(res)
-    })
-})
+const THUNK_PREFIXES = {
+  REGISTER: "auth/register",
+}
 
-const login = createAsyncThunk("auth/login", async (arg: LoginArgs) => {
-  try {
+const register = createAppAsyncThunk<any, RegisterArgs>(
+  THUNK_PREFIXES.REGISTER,
+  (arg) => {
+    AuthApi.register(arg)
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((res) => {
+        console.error(res)
+      })
+  },
+)
+
+const login = createAppAsyncThunk<{ user: User }, LoginArgs>(
+  "auth/login",
+  async (arg) => {
     const res = await AuthApi.login(arg)
     return { user: res.data }
-  } catch (e) {
-    console.error(e)
-  }
-})
+  },
+)
 
 const slice = createSlice({
   name: "auth",
-  initialState: { user: null as User | null },
+  initialState: { user: null as User | null, isLoading: false },
   reducers: {
     setUser: (state, action: PayloadAction<{ user: User }>) => {
       state.user = action.payload.user
     },
   },
   extraReducers: (builder) => {
+    builder.addCase(login.pending, (state) => {
+      state.isLoading = true
+    })
     builder.addCase(login.fulfilled, (state, action) => {
       if (action.payload?.user) {
         state.user = action.payload.user
+        state.isLoading = false
       }
+    })
+    builder.addCase(login.rejected, (state) => {
+      state.isLoading = false
     })
   },
 })
